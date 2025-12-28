8e9a1027ad6d4a8798a089ad6b14a6d7d75fc3b6
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-12-28 17:46:35 -0500

    Add `setup_logging` helper for configuring logging in test environment

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 pub mod assets;
 pub mod download_util;
+mod java;
 pub mod manifest_v2;
 pub mod sha_validation;
 pub mod version_manifest;
+
+#[cfg(test)]
+pub(crate) fn setup_logging() {
+    _ = pretty_env_logger::env_logger::builder().is_test(true).format_timestamp(None).filter_level(log::LevelFilter::Trace).try_init();
+}
